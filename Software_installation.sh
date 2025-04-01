c9d2502d69d30e7d9ba0fc1a098667d8723d503c
minimau <aishwarya.nikam3009@gmail.com>
2025-04-01 11:09:00 +0100

    Software_installation.sh
    Automate Software Installation

---
+++ Software_installation.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#Author: Aishwarya
+#Automate Software Installation
+
+packages="java-17* git tree httpd docker"
+
+echo "Updating package list..."
+sudo yum update -y
+echo "Installing required packages..."
+sudo yum install -y $packages
+echo "Installation complete! of $packages"
